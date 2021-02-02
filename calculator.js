a28ad8895869430787b636b7235a8e925dcc8d71
Mac <mac@Macs-MacBook-Pro.local>
2021-02-02 19:50:28 +0300

    task 4 completed

--- calculator.js
+++ calculator.js
@@ -1,3 +1,16 @@
+let integer1 = 2;
+let integer2 = 6;
+let operation = "/";
+
+if (operation === "+") {
+  console.log(integer1 + integer2);
+} else if (operation === "-") {
+  console.log(integer1 - integer2);
+} else if (operation === "*") {
+  console.log(integer1 * integer2);
+} else if (operation === "/") {
+  console.log(integer1 / integer2);
+}
 // Define the numbers and the operation here
 
 // Write if-statements here
